2abedb64d01aa2336b66222ec60f7dc1af119757
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-03-07 18:33:03 +0200

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+echo 'creating proper namespace' &&
+
+kubectl apply -f .infrastructure/namespace.yml 
+
+echo 'Creating a persistent volume' &&
+
+kubectl apply -f .infrastructure/pv.yml &&
+
+echo 'Applying claim' &&
+
+kubectl apply -f .infrastructure/pvc.yml &&
+
+echo 'Reapply deployment with new settings' &&
+
+kubectl apply -f .infrastructure/deployment.yml 
